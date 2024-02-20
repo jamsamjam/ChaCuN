1fe9dc9de246427e7576f5303fceeb06931ed35f
jamsamjam <andsunlit@gmail.com>
2024-02-20 18:45:40 +0100

    3.10 ing

---
+++ src/ch/epfl/chacun/Animal.java
@@ -0,0 +1,35 @@
+package ch.epfl.chacun;
+
+public record Animal(int id, Kind kind) {
+    /*private int id;
+    private Kind kind;
+
+    public Animal(int id, Kind kind) {
+        this.id = id;
+        this.kind = kind;
+    }*/
+
+    // can use static method : tileId of Zone (3.12)
+
+    public int tileId() {
+        return id;
+    }
+
+    public enum Kind {
+        MAMMOTH,
+        AUROCHS,
+        DEER,
+        TIGER;
+
+        // NullPointerExceptionif kindis null,
+        //IllegalArgumentExceptionif zoneIdis strictly negative.
+        public Kind () {
+            if ()
+                throw new NullPointerException ();
+            else if (this.id() )
+        }
+    }
+
+
+
+}
--- src/ch/epfl/chacun/Direction.java
+++ src/ch/epfl/chacun/Direction.java
@@ -1,10 +1,22 @@
 package ch.epfl.chacun;
 
+import java.util.List;
+
 public enum Direction {
     N,
     E,
     S,
     W;
 
+    public static final Direction[] AllDirections = values();
+    public static final List<Direction> ALL = List.of(AllDirections);
+    public static final int COUNT = ALL.size();
 
+    public Direction rotated(Rotation rotation){
+        return .add(rotation);
+    }
+
+    public Direction opposite() {
+
+    }
 }
