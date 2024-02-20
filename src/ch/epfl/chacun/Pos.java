2f4fa70c3b76d9f80ae90a0a30153e0227bdc6ca
G-Coder123 <74449738+G-Coder123@users.noreply.github.com>
2024-02-20 14:47:38 +0100

    3.11

---
+++ src/ch/epfl/chacun/Pos.java
@@ -0,0 +1,19 @@
+package ch.epfl.chacun;
+
+public record Pos(int x, int y) {
+    public static final Pos ORIGIN = new Pos(0, 0);
+
+    public Pos translated(int dX, int dY) {
+        return new Pos(x + dX, y + dY);
+    }
+
+    public Pos neighbor(Direction direction) {
+        return switch (direction) {
+            case N -> new Pos(x, y - 1);
+            case E -> new Pos(x + 1, y);
+            case S -> new Pos(x, y + 1);
+            case W -> new Pos(x - 1, y);
+            default -> throw new IllegalArgumentException("Invalid direction: " + direction);
+        };
+    }
+}
