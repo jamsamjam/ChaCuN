88fe5e99d773e2520b53681f05e05389228501b2
jamsamjam <andsunlit@gmail.com>
2024-02-23 12:59:42 +0100

    sam

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -33,9 +33,9 @@ public final class Submit {
     // CONFIGURATION
     // -------------
     // Jeton du premier membre du groupe
-    private static final String TOKEN_1 = "Gehna Yadav (379155)";
+    private static final String TOKEN_1 = "shai1Woo";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
-    private static final String TOKEN_2 = "Sam Lee (375535)";
+    private static final String TOKEN_2 = "Waeh0shu";
     // Noms des éventuels fichiers Java additionnels à inclure (p.ex. "MyClass.java")
     private static final List<String> ADDITIONAL_FILES =
             List.of();
