44ab29aaa53b1ecec46fe3588c2c1da038796b96
jamsamjam <andsunlit@gmail.com>
2024-02-27 09:31:48 +0100

    3.5.1 ing

--- src/ch/epfl/chacun/Tile.java
+++ src/ch/epfl/chacun/Tile.java
@@ -1,6 +1,35 @@
 package ch.epfl.chacun;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public record Tile(int id, Kind kind, TileSide n, TileSide e, TileSide s, TileSide w) {
+
+    /**
+     * @return the list of the four sides of the tile, in the order n, e, s, w
+     */
+    public List<TileSide> sides() {
+        return List.of(n, e, s, w);
+    }
+
+    /**
+     * @return the set of border areas of the tile (except lakes)
+     */
+    public Set<Zone> sideZones() {
+        Set<Zone> sideZones = new HashSet<>();
+        for (TileSide side : sides()) {
+            sideZones.addAll(side.zones());
+        }
+        return sideZones;
+    }
+
+    /**
+     * @return the set of all areas of the tile (including lakes)
+     */
+    public Set<Zone> zones() {
+        return
+    }
 
     /**
      * Lists the kinds of tiles that exist
