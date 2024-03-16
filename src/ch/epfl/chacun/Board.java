ea974723ccedd5d6a9387857fc47858c20ece961
G-Coder123 <74449738+G-Coder123@users.noreply.github.com>
2024-03-16 13:50:29 +0100

    gehna board

--- src/ch/epfl/chacun/Board.java
+++ src/ch/epfl/chacun/Board.java
@@ -1,6 +1,7 @@
 package ch.epfl.chacun;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Board {
@@ -9,9 +10,9 @@ public class Board {
     private final ZonePartitions zonePartitions;
     private final Set<Animal> canceledAnimals;
 
-    //TODO need to check if this is correct:
-    //public static final int REACH = 12; (is the reach actually supposed to be 12?)
-    //public static final Board EMPTY = new Board(new PlacedTile[625], new int[0], ZonePartitions.EMPTY, Set.of()); (is this how we create an empty board?)
+    //TODO need to check if REACH and EMPTY is correct
+    public static final int REACH = 12;
+    public static final Board EMPTY = new Board(new PlacedTile[625], new int[0], ZonePartitions.EMPTY, Set.of()); //is this how we create an empty board?
 
     private Board(PlacedTile[] placedTiles, int[] tileIndexes, ZonePartitions zonePartitions, Set<Animal> canceledAnimals) {
         this.placedTiles = placedTiles;
@@ -21,9 +22,13 @@ public class Board {
     }
 
     public PlacedTile tileAt(Pos pos) {
-        //TODO: what method to check if the position is valid? How can we check
-
-        return null; //only for now because this method is not finished
+        //TODO: is this index calculation valid? // do we just need to use tileIndexes here?
+        int index = (pos.y() + REACH) * (2 * REACH + 1) + (pos.x() + REACH);
+        if (index >= 0 && index < placedTiles.length) {
+            return placedTiles[index];
+        } else {
+            return null;
+        }
     }
 
     public PlacedTile tileWithId(int tileId) {
@@ -32,21 +37,143 @@ public class Board {
                 return placedTiles[i];
             }
         }
-        throw new IllegalArgumentException(); //does pre-conditions class need to be used here?
+        throw new IllegalArgumentException();
     }
     public Set<Animal> cancelledAnimals() {
-        return Set.copyOf(canceledAnimals); //here we can submit a new copyof or a new hashset
+        return Set.copyOf(canceledAnimals); //here we can submit a new copyof or a new hashset() ---does it make a difference?
     }
 
     public Set<Occupant> occupants() {
         Set<Occupant> allOccupants = new HashSet<>();
         for (PlacedTile tile : placedTiles) {
             if (tile != null) {
-                //TODO Add occupants here
+                //TODO check if it is the potential occupants we are adding here?
+                allOccupants.addAll(tile.potentialOccupants());
             }
         }
         return allOccupants;
     }
+
+    public Area<Zone.Forest> forestArea(Zone.Forest forest) {
+        //TODO: need to check the return statement here and if the condition in the if statement is correct
+        Set<Zone.Forest> foundForests = new HashSet<>();
+        for (PlacedTile tile : placedTiles) {
+            if (tile != null && tile.forestZones().contains(forest)) {
+                foundForests.add(forest);
+                return new Area<>(foundForests, List.of(), 0);
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public Area<Zone.Meadow> meadowArea(Zone.Meadow meadow) {
+        Set<Zone.Meadow> foundMeadows = new HashSet<>();
+        for (PlacedTile tile : placedTiles) {
+            if (tile != null && tile.meadowZones().contains(meadow)) {
+                foundMeadows.add(meadow);
+                return new Area<>(foundMeadows, List.of(), 0);
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public Area<Zone.River> riverArea(Zone.River riverZone) {
+        Set<Zone.River> foundRivers = new HashSet<>();
+        for (PlacedTile tile : placedTiles) {
+            if (tile != null && tile.riverZones().contains(riverZone)) {
+                foundRivers.add(riverZone);
+                return new Area<>(foundRivers, List.of(), 0);
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public Area<Zone.Water> riverSystemArea(Zone.Water water) {
+        Set<Zone.Water> foundWaters = new HashSet<>();
+        for (PlacedTile tile : placedTiles) {
+            //TODO: IF CONDOTION is missing here! im not sure what zone we are checking with riversystem!
+            if (tile != null) {
+                foundWaters.add(water);
+                return new Area<>(foundWaters, List.of(), 0);
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public Set<Area<Zone.Meadow>> meadowAreas() {
+        Set<Area<Zone.Meadow>> meadowAreas = new HashSet<>();
+        for (PlacedTile tile : placedTiles) {
+            if (tile != null) {
+                for (Zone.Meadow meadow : tile.meadowZones()) {
+                    meadowAreas.add(meadowArea(meadow));
+                }
+            }
+        }
+        return meadowAreas;
+    }
+
+    public Set<Area<Zone.Water>> riverSystemAreas() {
+        Set<Area<Zone.Water>> riverSystemAreas = new HashSet<>();
+        for (PlacedTile tile : placedTiles) {
+            if (tile != null) {
+                //TODO: are we going through the river zones here?
+                for (Zone.River river : tile.riverZones()) {
+                    riverSystemAreas.add(riverSystemArea(river));
+                }
+            }
+        }
+        return riverSystemAreas;
+    }
+
+    public Area<Zone.Meadow> adjacentMeadow(Pos pos, Zone.Meadow meadowZone) {
+        //TODO: Strugulling with this one!
+        return null; }
+
+    public int occupantCount(PlayerColor player, Occupant.Kind occupantKind) {
+        int count = 0;
+        for (PlacedTile tile : placedTiles) {
+            if (tile != null ) {
+                //TODO: For Gehna need to finish!
+                return 0;
+                }
+            }
+        return count;
+    }
+
+    public Set<Pos> insertionPositions() {
+        //TODO: need to check the for loops here -- in my opinion there should be two and they are both going through the reach
+        Set<Pos> positions = new HashSet<>();
+        for (int y = -Board.REACH; y <= Board.REACH; y++) {
+            for (int x = -Board.REACH; x <= Board.REACH; x++) {
+                Pos pos = new Pos(x, y);
+                PlacedTile tile = tileAt(pos);
+                if (tile == null) {
+                    positions.add(pos);
+                }
+            }
+        }
+        return positions;
+    }
+
+    public PlacedTile lastPlacedTile() {
+        for (int i = placedTiles.length - 1; i >= 0; i--) {
+            if (placedTiles[i] != null) {
+                return placedTiles[i];
+            }
+        }
+        return null;
+    }
+
+    public Set<Area<Zone.Forest>> forestsClosedByLastTile() {
+        //TODO strugulling with this one!  -- GEHNA tODO
+        return null; }
+
+    Set<Area<Zone.River>> riversClosedByLastTile() {
+        //TODO strugulling with this one!  -- GEHNA tODO
+        return null;
+    }
+
+
 
 
 }
